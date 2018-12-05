d783285d4c13609374ec47a9b83482d8b915c71f
Thomas Cruz <tcruz88@gmail.com>
2018-12-05 02:03:16 -0500

    update procedure

--- pages/doctor/doctor-rx.php
+++ pages/doctor/doctor-rx.php
@@ -127,8 +127,7 @@
 <?php
 $uid = $_SESSION['userid'];
 $conn = mysqli_connect("localhost","root","troublein421","HealthcareDB");
-$sql = "CALL ViewUpcomingScripts ('abc5-48dc-b830', '2');";
- 
+$sql = "CALL ViewALLScripts ('$uid', '4');"; 
 if($result=mysqli_query($conn,$sql)){
 	while($row=mysqli_fetch_assoc($result)) { ?>
  
