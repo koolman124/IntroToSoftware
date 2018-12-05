4be6849395f0191360c65bedef87ab32d5b0cd02
Jarman <jarman124@gmail.com>
2018-12-04 23:16:58 -0500

    Changed look of form in patient info

--- pages/doctor/doctor-medinfo.php
+++ pages/doctor/doctor-medinfo.php
@@ -426,16 +426,16 @@ if($result=mysqli_query($conn,$sql)){
             
          }else {
             ?>
-               <form method = "post" action = "<?php $_PHP_SELF ?>">
+               <form method = "post" action ="<?php $_PHP_SELF ?>">
                   <table width = "400" border =" 0" cellspacing = "1" 
                      cellpadding = "2">
                   
                      <tr>
                         <td width = "100">First Name</td>
-                        <td><input name="fname" type="text" id="fname" ></td>
+                        <td><input name="fname" type="text" id="fname" class="form-control"></td>
                      </tr>
                         <td>
-                           <input name = "update" type = "submit" id = "update" value = "Update">
+                           <button name="update" class="btn btn-primary btn-block" type ="submit" id="update" value ="Update"> Update </button>
                         </td>
                      </tr>
                   
