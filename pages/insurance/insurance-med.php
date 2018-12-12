5f5a2078f79a91158f3fc0a5acd656391f66f4d7
Jarman Liu <jarman124@gmail.com>
2018-12-12 15:27:24 -0500

    Code cleanup

--- pages/insurance/insurance-med.php
+++ pages/insurance/insurance-med.php
@@ -233,8 +233,8 @@ $row = mysqli_fetch_assoc($result);
     <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
     <script src="../../vendor/chart.js/Chart.min.js"></script>
-    <script src="../../vendor/datatables/jquery.dataTables.js"></script>
-    <script src="../../vendor/datatables/dataTables.bootstrap4.js"></script>
     <script src="../../js/sb-admin.min.js"></script>
-    <script src="../../js/demo/datatables-demo.js"></script>
-    <script src="../../js/demo/chart-area-demo.js"></script>
+
+  </body>
+
+</html>
